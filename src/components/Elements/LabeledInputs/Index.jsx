d8908f938025869856586c2dd75182dbc49b57be
IndraFadillahCODE <indrafadillahtugas@gmail.com>
2024-10-14 20:19:32 +0700

    Updated design

---
+++ src/components/Elements/LabeledInputs/Index.jsx
@@ -0,0 +1,15 @@
+import Input from "./Input";
+import Label from "./Label";
+
+const LabeledInputs = (props) => {
+  const { label, name, type, placeholder } = props;
+
+  return (
+    <>
+      <Label htmlFor={name}>{label}</Label>
+      <Input name={name} type={type} placeholder={placeholder} />
+    </>
+  );
+};
+
+export default LabeledInputs;
---
+++ src/components/Fragments/FormSignIn.jsx
@@ -0,0 +1,33 @@
+import LabeledInput from "../Elements/LabeledInputs/Index";
+import CheckBox from "../Elements/CheckBox/Index";
+import Button from "../Elements/Button/Index";
+const FormSignIn = () => {
+    return (
+      <form action="">
+        <div className="mb-6">
+          <LabeledInput
+            label="Email address"
+            type="email"
+            placeholder="hello@example.com"
+            name="email"
+          />
+        </div>
+        <div className="mb-6">
+          <LabeledInput
+            label="Password"
+            type="password"
+            placeholder="*************"
+            name="password"
+          />
+        </div>
+        <div className="mb-3">
+          <CheckBox label="Keep me signed in" name="status" />
+        </div>
+        <Button variant="bg-primary w-full text-white" type="submit" >
+            Login
+        </Button>
+      </form>
+    );
+  };
+  
+  export default FormSignIn;
