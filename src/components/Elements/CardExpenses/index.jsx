5070f43762c94aba43717eac29e22a2f31b2a9b7
IndraFadillahCODE <indrafadillahtugas@gmail.com>
2024-11-04 17:05:10 +0700

    expenses fix

--- src/components/Elements/CardExpenses/index.jsx
+++ src/components/Elements/CardExpenses/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CardExpenses = () => {
   return (
     <>
-      <div className="bg-white rounded-lg px-6 py-5 shadow-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni neque quam alias enim ullam deserunt ducimus consequatur cumque hic voluptatem totam ab quibusdam facilis distinctio ratione, sequi vel porro eum.</div>
+      <div className="bg-white rounded-lg px-6 py-5 shadow-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mollis suscipit suscipit. Maecenas semper risus leo, vitae vehicula risus congue vitae. Sed semper sem est, in malesuada tellus placerat ac. Quisque auctor lobortis erat, eget fringilla velit cursus vel. In hac habitasse platea dictumst. Duis et velit turpis. Mauris nec orci eu orci interdum laoreet sed eget velit. Proin ultricies augue eu nibh porta cursus. Donec cursus nisi eu urna interdum, in porttitor arcu posuere. Quisque pretium dui quis lorem ultrices aliquet. Vivamus vitae venenatis ante. Vestibulum est risus, sollicitudin eget nulla eu, egestas iaculis nibh. Morbi aliquam nulla nec nibh tincidunt sollicitudin.</div>
     </>  )
 }
 
